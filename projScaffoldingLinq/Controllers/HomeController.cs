476e1e1aba01ef35f76a3d2fca5a46d2f89ee8f6
sagar <sagurana.sa143@gmail.com>
2019-06-22 17:29:03 +0530

    Add project files.

---
+++ projScaffoldingLinq/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Linq;
+using System.Configuration;
+//using projScaffoldingLinq.Models;
+
+namespace projScaffoldingLinq.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        DataContext dc = new DataContext(ConfigurationManager.ConnectionStrings["cnndbStudent"].ToString());
+        public ActionResult Index()
+        {
+            IEnumerable<StudMst> objStud = dc.GetTable<StudMst>();
+            return View(objStud);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(StudMst obj)
+        {
+            dc.GetTable<StudMst>().InsertOnSubmit(obj);
+            dc.SubmitChanges();
+            return View();
+        }
+        [HttpGet]
+        [ActionName("Edit")]
+        public ActionResult Edit_get(int id)
+        {
+            StudMst stud = dc.GetTable<StudMst>().Single(x => x.id == Convert.ToInt32(id));
+            return View(stud);
+        }
+        [HttpPost]
+        [ActionName("Edit")]
+        public ActionResult Edit_post(int id)
+        {
+            StudMst stud= dc.GetTable<StudMst>().Single(x => x.id == Convert.ToInt32(id));
+               UpdateModel(stud);
+               dc.SubmitChanges();
+            return View();
+        }
+        [HttpGet]
+        [ActionName("Delete")]
+        public ActionResult Del_get(int? id)
+        {
+            StudMst stud = dc.GetTable<StudMst>().Single(x => x.id == Convert.ToInt32(id));
+            return View(stud);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult Del_post(int id)
+        {
+            StudMst stud = dc.GetTable<StudMst>().Single(x => x.id == Convert.ToInt32(id));
+            dc.GetTable<StudMst>().DeleteOnSubmit(stud);
+            dc.SubmitChanges();
+            return View();
+        }
+
+    }
+}
---
+++ projScaffoldingLinq/Models/StudMst.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Web;
+
+namespace projScaffoldingLinq.Models
+{
+    [Table(Name="StudMst")]
+    public class StudMst
+    {
+        public int ID { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Course { get; set; }
+        public int? Fees { get; set; }
+        public DateTime? Doj { get; set; }
+    }
+}
